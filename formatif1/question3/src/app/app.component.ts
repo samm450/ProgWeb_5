53bd9e54f95df46d51ccc2227b069616f25b0e1b
Samuel Coache <6250874@cegepmontpetit.ca>
2025-10-09 11:22:39 -0400

    fini question 3

--- formatif1/question3/src/app/app.component.ts
+++ formatif1/question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup, MinLengthValidator } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup, MinLengthValidator, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,31 +17,54 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 })
 export class AppComponent {
   title = 'reactive.form';
-
-
   formGroup : FormGroup;
+
   constructor(private formBuilder : FormBuilder) { 
     this.formGroup = this.formBuilder.group(
       {
         name : ['', [Validators.required]],
         roadnumber : ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
         postalcode : ['', [Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")] ],
-        comments : ['', [Validators.minLength(10), ]]
-      }
-    )
-
-    
+        comments : ['', [this.Commentaire()]]
+      },
+      { validators: this.nomCommentaire() }
+    );
   }
-  export function Commentaire() : ValidationFn{
-    
+
+  Commentaire() : ValidatorFn{
     return (control : AbstractControl) : ValidationErrors | null => {
-      const comment = control.get('comments')
-      if (!comment?.value){
-        // a continuer
+      const comment = control.value;
+
+      if(!comment){
+        return null
       }
-      else return null
+
+      const estvalide = comment.split(' ').length >= 10;
+      
+      return estvalide ? null : {Commentaire : true};
+    
     }
   }
+
+  nomCommentaire(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      // On récupère les valeurs de nos champs textes
+      const comment = control.get('comments');
+      const nom = control.get('nom');
+      // On regarde si les champs sont remplis avant de faire la validation
+      if (!comment?.value || !nom?.value) {
+        return null;
+      }
+      // On fait notre validation
+      const estValide = !comment.value.includes(nom.value);
+
+      if (!estValide) {
+        // On ajoute l'erreur pour l'afficher sous le champ courriel
+        // On conserve les autres erreurs déjà présentes
+        comment.setErrors({ ...comment.errors, nomCommentaire : true });
+      }
+      return estValide ? null : { nomCommentaire: true };
+    };
+  }  
+  
 }
-
-
